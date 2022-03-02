1e3c4a0cb6d90cfde6e367ffb49ffeef642915bc
energistix <50068022+energistix@users.noreply.github.com>
2022-03-02 21:13:14 +0100

    done proper selecting of tile for connect4

--- jeux/puissance4/Cell.js
+++ jeux/puissance4/Cell.js
@@ -1,18 +1,23 @@
 class Cell {
   constructor(x, y, game) {
+    this.x = x
+    this.y = y
+    this.isFilled = false
+
     this.img = document.createElement("img")
     this.img.style.left = `${x}rem`
     this.img.style.top = `${y}rem`
     this.img.src = "./assets/slot.png"
 
     this.img.addEventListener("mouseover", () => {
-      game.mouseOver(x, y)
+      game.mouseOver(this)
     })
 
     document.body.appendChild(this.img)
   }
 
   set color(color) {
-    this.img.style.color = color
+    console.log(color)
+    this.img.style.backgroundColor = color
   }
 }
